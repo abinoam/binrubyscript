b45d8ff38f28686ebf35c2c08c8552ef4c46d04c
Abinoam Praxedes Marques Jr. <abinoam@gmail.com>
2024-10-13 15:41:46 -0300

    Use RustEmbed to load Ruby code into the project

---
+++ src/embedded_ruby/binrubyscript.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+
+puts "Hello World from embedded Ruby (with Artichoke)!"
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use rust_embed::RustEmbed;
+
+// The Ruby source code to be embedded
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord, RustEmbed)]
+#[folder = "src/embedded_ruby"]
+pub struct Sources;
+
 fn main() {
     println!("Hello, world from pure Rust!");
 }
